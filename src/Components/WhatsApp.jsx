a9105f225a9a18a5b092c7cd7a1145ef255f30e4
Juan <juanocju@gmail.com>
2023-10-12 12:44:32 -0600

    Cambio de número

--- src/Components/WhatsApp.jsx
+++ src/Components/WhatsApp.jsx
@@ -7,7 +7,7 @@ const WhatsApp = () => {
   return (
     <Box>
       <Button onClick={() => {
-        window.open('https://wa.me/557621286506?text=Hola')
+        window.open('https://wa.me/+527772062914?text=Hola')
       }}>Test</Button>
       <ReactWhatsapp number="55-762-128-6506" message="Hello World!!!">
         Mandar mensaje
